5ea7826e414941ef8749356f146942ab40cd356f
KERNEL01 <richard.d.connell@gmail.com>
2022-07-15 13:43:30 +1000

    Defined more of resource subsystem, entity baseclass.

--- client/objects/entity.py
+++ client/objects/entity.py
@@ -7,5 +7,25 @@ class Entity:
     def __init__(self):
         self.uid = uuid.uuid4()
         self.name = 'NewEntity'
-        self.image = pygame.Surface
+
+        self.should_be_drawn = True # will the renderer blit the image to the screen
+        self.active = True # should it consume during an update tick
+
+        self.image = pygame.Surface(0,0) # todo get this from the resource manager
+        self.rect = self.image.get_rect() # todo get this from the resource manager
+
+        self.location = self.locX, self.locY = 0, 0 # world position
+
+        self.collider_type = 'standard'
+
+        self.health = 100
+        self.can_be_killed = False
+
+        self.can_be_used = False
+
+        self.speed = 0
+        self.acceleration = 0
+        self.friction = 0
+        self.gravity = 0
+
         # todo continue defining Entity
--- client/subsystems/resource.py
+++ client/subsystems/resource.py
@@ -8,12 +8,29 @@ from client.subsystems import subsystem, configuration
 
 @singleton.Singleton
 class Resource(subsystem.Subsystem):
+    """ Resource Subsystem stores resources for use by the entire system.
 
+     images, the frame offsets for that image
+     sounds, music
+
+     objects, entities are able to reference the name of the sound or image for use in processing, and the object
+     stores how to use the resource (sound level, frame of the animation to render at the time.)
+
+     """
     def __init__(self):
         super().__init__()
         self.datastore = None
         self.icon = None
         self.resource_store = None
+
+        self.image = {
+            "image_name": {"image": "Surface",
+                           "frames": {0: "Frame"}}
+        }
+
+        self.sound = {
+            "sound_name": "Sound"
+        }
 
     def on_active(self):
         self.datastore = os.path.join(configuration.Configuration.instance().install_path, 'resources')
